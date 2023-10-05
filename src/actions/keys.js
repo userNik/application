4df0922932227e079413db376d53db0c5f3e32f8
Igor Nikonenko <igorniknet@gmail.com>
2023-10-05 16:55:24 +0300

    Delete src directory

--- src/actions/keys.js
+++
@@ -1,2 +0,0 @@
-export const ADD_MOVIE = "ADD_MOVIE";
-export const CLEAR_LIST = "CLEAR_LIST";
--- src/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk"
-import movies from "../reducers/movies";
-
-export default createStore(movies, applyMiddleware(thunkMiddleware));
